5cc7309d09ada0314540fb0ea6151e220b687dd2
英子 <@192.168.0.16>
2018-05-26 10:33:08 +0900

    コミットします。

--- src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
+++ src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
@@ -7,36 +7,36 @@ import java.io.IOException;
 import java.util.Calendar;
 import java.util.Properties;
 
-import jp.gihyo.jenkinsbook.page.ResultPage;
-import jp.gihyo.jenkinsbook.page.TopPage;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import jp.gihyo.jenkinsbook.page.ResultPage;
+import jp.gihyo.jenkinsbook.page.TopPage;
 
 public class SampleTestCase {
 
 	private static Properties prop = new Properties();
 	private static WebDriver driver;
-	
+
 	@BeforeClass
 	public static void setUpClass() throws IOException {
 		prop.load(new FileInputStream("target\\test-classes\\selenium.properties"));
+		System.setProperty("webdriver.gecko.driver", "C:/project/geckodriver.exe");
 		driver = new FirefoxDriver();
 	}
-	
+
 	@AfterClass
 	public static void tearDownClass() throws IOException {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void testNormal01() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage topPage = new TopPage(driver);
 		assertEquals("名字", topPage.getLastNameLabel());
 		assertEquals("名前", topPage.getFirstNameLabel());
@@ -45,16 +45,16 @@ public class SampleTestCase {
 		assertTrue(topPage.hasFirstNameInput());
 		assertTrue(topPage.hasSubmit());
 	}
-	
+
 	@Test
 	public void testNormal02() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage topPage = new TopPage(driver);
 		topPage.setLastName("Hoge");
 		topPage.setFirstName("Foo");
 		topPage.submit();
-		
+
 		String greeting = "";
 		Calendar calendar = Calendar.getInstance();
 		int hour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -66,15 +66,15 @@ public class SampleTestCase {
 		ResultPage resultPage = new ResultPage(driver);
 		assertEquals(greeting + ", Hoge Foo!!", resultPage.getText());
 	}
-	
+
 	@Test
 	public void testError01() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage page = new TopPage(driver);
 		page.setFirstName("Hoge");
 		page.submit();
-		
+
 		ResultPage resultPage = new ResultPage(driver);
 		assertEquals("エラー", resultPage.getText());
 	}
@@ -82,11 +82,11 @@ public class SampleTestCase {
 	@Test
 	public void testError02() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage page = new TopPage(driver);
 		page.setLastName("Hoge");
 		page.submit();
-		
+
 		ResultPage resultPage = new ResultPage(driver);
 		assertEquals("エラー", resultPage.getText());
 	}
